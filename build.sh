3aca383b5d47951b6ffc503d4b96c6cd2a05d157
Fabian Deutsch <fabiand@fedoraproject.org>
2015-09-10 08:31:11 +0200

    Allow multiple ovirt release
    Signed-off-by: Fabian Deutsch <fabiand@fedoraproject.org>

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 set -xe
 
-OVIRTVER=3.5
+OVIRTVERS=3.5
 DSTDIR=$2
 
 sudo yum install -y docker
@@ -10,26 +10,36 @@ sudo service docker start
 git clone https://github.com/jasonbrooks/byo-atomic.git
 pushd byo-atomic
 
-# Clone the defs
-git clone https://github.com/fabiand/sig-atomic-buildscripts.git node
-pushd node
-  git checkout ovirt-host
-  make OVIRTVER=$OVIRTVER
-popd
-
 # Build the build-env
 sudo docker build --rm -t $USER/atomicrepo .
 sudo docker rm -f -v atomicrepo || :
 sudo docker run --privileged -v $PWD:/builddir -d --name atomicrepo $USER/atomicrepo
 incontainer() { sudo docker exec atomicrepo $@ ; }
 
-# Build the tree
-incontainer cp -v /builddir/node/RPM-GPG-ovirt /etc/pki/rpm-gpg/RPM-GPG-ovirt-$OVIRTVER
-incontainer rpm-ostree compose tree --repo=/srv/rpm-ostree/repo/ /builddir/node/centos-ovirt-host.json
-incontainer du -hs /srv/rpm-ostree
-sudo docker cp atomicrepo:/srv/rpm-ostree .
 
-if [[ -n "$DSTDIR" ]]; then
-  rsync -PHvarc rpm-ostree/repo "$DSTDIR"
-  rm -rf rpm-ostree
-fi
+for OVIRTVER in $OVIRTVERS;
+do
+  echo Building repo for "$OVIRTVER"
+
+  # Clone the defs
+  if [[ ! -e node ]]; then
+    git clone https://github.com/fabiand/sig-atomic-buildscripts.git node
+    git checkout ovirt-host
+  fi
+  pushd node
+    git clean -fdx
+    git reset --hard ovirt-host
+    make OVIRTVER=$OVIRTVER
+  popd
+
+  # Build the tree
+  incontainer cp -v /builddir/node/RPM-GPG-ovirt /etc/pki/rpm-gpg/RPM-GPG-ovirt-$OVIRTVER
+  incontainer rpm-ostree compose tree --repo=/srv/rpm-ostree/repo/ /builddir/node/centos-ovirt-host.json
+  incontainer du -hs /srv/rpm-ostree
+  sudo docker cp atomicrepo:/srv/rpm-ostree .
+
+  if [[ -n "$DSTDIR" ]]; then
+    rsync -PHvarc rpm-ostree/repo "$DSTDIR"
+    rm -rf rpm-ostree
+  fi
+done
